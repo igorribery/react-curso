7ec0088b85940495080eb58a19ff1501e02e65b4
Igor <(igorrds1998@gmail.com)>
2022-03-19 01:03:34 -0300

    criando eventos onClick

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
-import { Header } from './components/Header';
-import { Photo } from './components/Photo';
-
-
 const App = () => {  
+
+  const handleClick = () => {
+    alert('O botão foi clicado!');
+  }
+
+
   return (    
     <div>
-      <Header title="Este é um exemplo" />
-      <Header title="Este é um exemplo 2.0" />
-
-      Olá Mundo
-
-      <Photo legend="google">
-          <img src="https://www.google.com.br/google.jpg" alt="" />
-      </Photo>
+      <button onClick={handleClick}>Clique Aqui</button>
 
     </div>
   );
