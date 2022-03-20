2d44ee3ca6a7090ed529b40c8cf6498941fff584
Igor <(igorrds1998@gmail.com)>
2022-03-19 23:55:01 -0300

    exercicio criando form

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
-import React, { useState } from 'react';
+import { useState } from "react";
 
-const App = () => {  
-  const [name, setName] = useState('');
-  
-  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName( e.currentTarget.value );
+type HandleInputChange = React.ChangeEvent<HTMLInputElement>;
+
+const App = () => {
+
+  const [name, setName] = useState('João');
+  const [lastName, setLastName] = useState('Silva');
+  const [age, setAge] = useState(18);
+
+  const handleName = (e: HandleInputChange) => {
+    setName( e.target.value );
   }
-
+  const handleLast = (e: HandleInputChange) => {
+    setLastName( e.target.value );
+  }
+  const handleAge = (e: HandleInputChange) => {
+    setAge( parseInt(e.target.value ));
+  }
   return (
     <div>
-    Nome:
-    <input type='text' value={name} onChange={handleInput} />
-    <hr/>
-    Seu nome é: {name}
+      <div>
+        Nome:
+        <input type="text" value={name} onChange={handleName} />
+      </div>
 
+      <div>
+        Sobrenome:
+        <input type="text" value={lastName} onChange={handleLast} />
+      </div>
+
+      <div>
+        Idade:
+        <input type="text" value={age} onChange={handleAge} />
+      </div>
+
+      <hr/>
+
+      Olá {`${name} ${lastName}`}, tudo bem?<br/>
+      Você tem {age} anos.
     </div>
   );
-
 }
-
 
 export default App;
