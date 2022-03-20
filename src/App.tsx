b020b703726e385b26f571bc766c09b539ddbd4b
Igor <(igorrds1998@gmail.com)>
2022-03-19 22:47:00 -0300

    criando um input com state

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 const App = () => {  
-  const [n, setN] = useState(0);
-
-  const handleMinus = () => {
-    if(n > 0) {
-    setN( n - 1 );
-    }
-  }
-  const handleMore = () => {
-    setN ( n + 1);
+  const [name, setName] = useState('');
+  
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName( e.currentTarget.value );
   }
 
-  return (    
+  return (
     <div>
-      <button onClick={handleMinus}>-</button>
-      <div>{n}</div>
-      <button onClick={handleMore}>+</button>
+    Nome:
+    <input type='text' value={name} onChange={handleInput} />
+    <hr/>
+    Seu nome é: {name}
+
     </div>
   );
+
 }
+
 
 export default App;
