97fb29662e08ab294c4aeb257fd25c71c4c5354b
Igor <(igorrds1998@gmail.com)>
2022-03-24 17:10:49 -0300

    responsividade no tailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 const App = () => {
   
   return (
-    <div className='bg-blue-700 text-white p-3 w-auto font-bold'>
+    <div className='bg-blue-700 text-white p-4 text-2x1 font-bold flex flex-col text-center md:flex-row md:text-green-300 lg:text-red-400 lg:justify-between'>
       Texto qualquer
 
-    <button className='bg-green-400 px-2 ml-4 rounded-xl'>Clique Aqui</button>
+    <button className='bg-green-300 text-black p-2 rounded-xl mt-3 md: mt-0 '>Clique Aqui</button>
     </div>
   );
 }
