bc40ce632c9ed0989b7652288013f18a57cb6407
Igor <(igorrds1998@gmail.com)>
2022-03-29 00:09:56 -0300

    parte visual projeto calcImc

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import styles from './App.module.css';
+import poweredImage from './assets/powered.png';
+import { levels, calculateImc } from './helpers/imc';
+import { GridItem } from './components/GridItem';
+
+const App = () => {
+  
+    const [heightField, setHeightField] = useState<number>(0);
+    const [weightField, setWeightField] = useState<number>(0);
+
+    const handleCalcButton = () => {
+      if(heightField && weightField) {
+
+      } else {
+        alert('Preencha todos os campos.');
+      }
+    };
+    
+  return (
+      <div className={styles.main}>
+        <header>
+          <div className={styles.headerContainer}>
+            <img src={poweredImage} alt='' width={150} />
+          </div>
+          <div className={styles.container}>
+            <div className={styles.leftSide}>
+              <h1>Calcule seu IMC</h1>
+              <p>IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela Organização Mundial de Saúde para calcular o peso ideal de cada pessoa.</p>
+
+              <input 
+                type='number'
+                placeholder='Digite sua altura. Ex: 1.5 (em metros)'
+                value={heightField > 0 ? heightField : ''}
+                onChange={e => setHeightField(parseFloat(e.target.value))}
+              />
+
+              <input 
+                type='number'
+                placeholder='Digite seu peso. Ex: 73.3 (em kg)'
+                value={weightField > 0 ? weightField : ''}
+                onChange={e => setWeightField(parseFloat(e.target.value))}
+              />
+              
+              <button onClick={handleCalcButton}>Calcular</button>
+
+            </div>
+            <div className={styles.rightSide}>
+              <div className={styles.grid}>
+                {levels.map((item, key) => (
+                  <GridItem key={key} item={item}/>
+                ))}
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
+  );
+
+}
+
+export default App;
